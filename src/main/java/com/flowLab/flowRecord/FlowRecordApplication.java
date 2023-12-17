921eecbe26b821816d53b2a82ad957117c8361c6
bickwnag123@gmail.com <bickwang123@gmail.com>
2023-12-17 13:12:36 +0900

    init

---
+++ src/main/java/com/flowLab/flowRecord/FlowRecordApplication.java
@@ -0,0 +1,13 @@
+package com.flowLab.flowRecord;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowRecordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlowRecordApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/flowLab/flowRecord/FlowRecordApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flowLab.flowRecord;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowRecordApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
